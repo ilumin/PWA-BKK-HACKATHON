3c932f601184728b44b5990a631bfd2a717c436a
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-06-24 00:47:55 +0700

    ADD autoload test

--- play/index.js
+++ play/index.js
@@ -1,10 +1,8 @@
 import Vue from 'vue'
 import { play } from 'vue-play'
-import MyButton from './../src/components/MyButton.vue'
 
-Vue.component('my-button', MyButton)
+const load = requireContext => {
+  return requireContext.keys().map(requireContext)
+}
 
-play('CustomButton')
-  .add('with text', {
-    template: `<my-button @click="$log('123')" label="hello">Hello</my-button>`
-  })
+load(require.context('./', true, /.play.js$/))
