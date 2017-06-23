1a2019657dbc98c402ff3c2aa658d34753354e43
Wanichnun Sinpitak <kitaboons@gmail.com>
2017-06-24 01:32:03 +0700

    DONE error message

---
+++ play/error-message.play.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import { play } from 'vue-play'
+import ErrorMessage from './../src/components/ErrorMessage.vue'
+
+Vue.component('error-message', ErrorMessage)
+
+play('ErrorMessage')
+  .add('with text', {
+    template: `<error-message message="error tee mee ngoo"></error-message>`
+  })
