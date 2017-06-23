8b9f0cbf502c8346f6c73889ddfa05fc3f150951
Palakorn <palakorn.plo@ascendcorp.com>
2017-06-24 03:05:39 +0700

    Update

---
+++ src/util/LocationAutoComplete.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="location-auto-complete" @keyup.enter="autoComplete(message)">
+        <input v-model="message">
+        <div v-for="prediction in predictions" key="$index">{{ prediction.description }}</div>
+    </div>
+</template>
+
+<script>
+  var predictions =
+  export default {
+    name: 'locationAutoComplete',
+    props: ['message'],
+    methods: {
+      autoComplete: function (message) {
+        console.log(message, '<=== msg')
+      }
+    }
+  }
+</script>
