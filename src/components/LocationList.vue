70fc55ec3196f0aae97768d14d5a31a8ecf21411
Wanichnun Sinpitak <kitaboons@gmail.com>
2017-06-24 13:19:04 +0700

    DONE remove location from list

--- src/components/LocationList.vue
+++ src/components/LocationList.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="location-list">
-        <location v-for="location in locations" 
+        <location @locationId="locationId(location.id)" v-for="location in locations" 
             :location=location
             :key=location.id></location>
   </div>
@@ -13,6 +13,20 @@ export default {
     props: ['locations'],
     components: {
         Location
+    },
+    methods: {
+        locationId: function(id) {
+            var vm = this;
+            for (var i = 0; i < vm.locations.length; i++) {
+                if (vm.locations[i].id == String(id)) {
+                    console.log(vm.locations[i]);
+                    vm.locations.splice(i, 1);
+                    break;
+                }
+            }
+            console.log('id = '+ id)
+            //console.log(JSON.stringify(vm.locations))
+        }
     }
 }
 </script>
