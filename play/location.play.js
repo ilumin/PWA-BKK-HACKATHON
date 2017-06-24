783cc541d9f16f9ecb051146fad439e5b07790e3
Wanichnun Sinpitak <kitaboons@gmail.com>
2017-06-24 09:51:57 +0700

    ADD star-rating style

--- play/location.play.js
+++ play/location.play.js
@@ -6,5 +6,5 @@ Vue.component('location', Location)
 
 play('Location')
   .add('with photo_reference', {
-    template: `<location rating=3 location_name="location name tee mee ngoo" photo_reference="CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU"></location>`
+    template: `<location :rating=3.9 location_name="location name tee mee ngoo" photo_reference="CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU"></location>`
   })
--- src/components/Location.vue
+++ src/components/Location.vue
@@ -3,20 +3,25 @@
         <div id="location-photo" class="location-photo">
             <img :src="getImage(photo_reference, maxwidth, maxheight)"></img>
         </div>
-        <div id="location-name" class="location-name">
-            <p>{{ location_name }}</p>
-        </div>
-        <div>
-            <p>
-                {{ rating }}
-            </p>
+        <div id="location-info" class="location-info">
+            <div id="location-name" class="location-name">
+                <p>{{ location_name }}</p>
+            </div>
+            <div id="star-rating">
+                <star-rating :rating=rating :read-only=true :star-size="15" :increment="0.5"></star-rating>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
 import { getPlaceImage } from '../utils/ApiUtil';
+import StarRating from 'vue-star-rating';
+
 export default {
+    components: {
+        StarRating
+    },
     props: {
         location_name: String, 
         photo_reference: String, 
@@ -40,8 +45,12 @@ export default {
 <style>
 .location-photo {
     float: left;
+    padding: 5px;
 }
-.location-name {
-    float: right;
+.location-info {
+    padding: 5px;
+    position: inherit
 }
+.location-name {}
+.star-rating {}
 </style>
