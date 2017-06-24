c36db135704431e4f4d0527b46f7514ca36172e6
Palakorn <palakorn.plo@ascendcorp.com>
2017-06-24 22:33:07 +0700

    add trip

--- src/utils/ApiUtil.js
+++ src/utils/ApiUtil.js
@@ -1,17 +1,19 @@
 import axios from 'axios'
 
+var keyApi = 'AIzaSyDGvRoeZwvvbOmrQShZaAaQSjrtbwTLCWA'
+
 export function getPlaceImage (photoReference, maxWidth, maxHeight) {
   return 'https://maps.googleapis.com/maps/api/place/photo?maxheight=' + maxHeight + '&maxwidth=' + maxWidth + '&photoreference=' + photoReference + '&key=AIzaSyDGvRoeZwvvbOmrQShZaAaQSjrtbwTLCWA'
 }
 
 export function autoComplete (value) {
-  return axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${value}&types=geocode&key=AIzaSyAMJ1pWSfYUWu7_Jj7lUy6QY3pwdBONXwk`)
+  return axios.get(`https://maps.googleapis.com/maps/api/place/autocomplete/json?input=${value}&types=geocode&key=${keyApi}`)
 }
 
 export function getPlaceDetail (placeId) {
-  return axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=AIzaSyAMJ1pWSfYUWu7_Jj7lUy6QY3pwdBONXwk`)
+  return axios.get(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${placeId}&key=${keyApi}`)
 }
 
 export function searchPlace (q) {
-  return axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${q}&key=AIzaSyAMJ1pWSfYUWu7_Jj7lUy6QY3pwdBONXwk`)
+  return axios.get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${q}&key=${keyApi}`)
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,6 +20,7 @@
 import MyButton from '@/components/MyButton.vue'
 import AutoComplete from '@/utils/LocationAutoComplete.vue'
 import {searchPlace} from '@/utils/ApiUtil'
+import {getUser, db} from '@/utils/FirebaseApp'
 
 export default {
   name: 'home',
@@ -42,25 +43,58 @@ export default {
         alert('Please Input StartAt and Destination')
         return
       }
-
+      var userData = getUser()
+      var locations = {}
       searchPlace(vm.startAt).then(function (response) {
         if (response.data.results) {
-//
-//
-//
-//          vm.$firebaseRefs.trips.push(
-//            data
-//          ).then(() => {
-//
-//          })
+          locations[response.data.results[0].place_id] = {
+            name: response.data.results[0].name,
+            order: 0,
+            rating: (response.data.results[0].rating) ? response.data.results[0].rating : 0,
+            thumbnail: (response.data.results[0].photos[0].photo_reference) ? response.data.results[0].photos[0].photo_reference : ''
+          }
         }
+      }).then(function (rrr) {
+        searchPlace(vm.destination).then(function (response) {
+          if (response.data.results) {
+            locations[response.data.results[0].place_id] = {
+              name: response.data.results[0].name,
+              order: 0,
+              rating: (response.data.results[0].rating) ? response.data.results[0].rating : 0,
+              thumbnail: (response.data.results[0].photos[0].photo_reference) ? response.data.results[0].photos[0].photo_reference : ''
+            }
+          }
+        })
+      }).then(function () {
+        console.log(locations, '<<<<<<<<< locations')
+        var data = {
+          name: vm.startAt + ' - ' + vm.destination,
+          date: '2017-06-24 11:00:00',
+          locations: locations
+        }
+        db.ref('/trips/' + userData.uid).push(
+          data
+        ).then(function () {
+          console.log('thennnn')
+        })
       })
+
+//      searchPlace(vm.destination).then(function (response) {
+//        if (response.data.results) {
+//
+//        }
+//      })
     },
     getStartAt: function (val) {
       this.startAt = val
     },
     getDestination: function (val) {
       this.destination = val
+    },
+    firebase: function () {
+      return {
+        trips: db.ref('trips')
+      }
     }
   }
 }
