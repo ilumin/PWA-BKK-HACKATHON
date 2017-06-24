44d9aa977dd3ce4a63a4e07a376f09fc704f2515
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-06-24 23:31:22 +0700

    UPDATE render map in trip

--- src/views/TripDetail.vue
+++ src/views/TripDetail.vue
@@ -1,24 +1,38 @@
 <template>
   <div>
     <h1>Trip Detail page {{ trip_id }}</h1>
-    <pre>{{ trip }}</pre>
+    <template v-if="showMap">
+      <GMap :direction="direction"></GMap>
+    </template>
+    <pre>{{ trip.locations }}</pre>
   </div>
 </template>
 
 <script>
-import {getUser, db} from '@/utils/FirebaseApp';
+import {getUser, db} from '@/utils/FirebaseApp'
+import Map from '@/components/Map'
 
 export default {
   name: 'TripDetail',
   props: ['trip_id'],
+  components: {
+    "GMap": Map
+  },
   data: () => ({
-    trip: {}
+    trip: {},
+    direction: false,
+    showMap: false
   }),
   firebase () {
     return {
       trip: {
         source: db.ref('trips/' + getUser().uid + '/' + this.trip_id),
         asObject: true,
+        readyCallback: () => {
+          this.showMap = true
+          this.direction = Object.keys(this.trip.locations)
+            .map(key => this.trip.locations[key])
+        }
       }
     }
   }
