7152cd57ef8530b337fa1292d6840aaed3a804bb
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-06-24 11:09:33 +0700

    ADD map spec

---
+++ src/components/Map.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="components map">
+    display map
+    <pre>{{ markers }}</pre>
+    <pre>{{ direction }}</pre>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Map',
+  props: ['markers', 'direction'],
+  mounted () {
+    console.log('markers:', this.markers)
+    console.log('direction:', this.direction)
+  }
+}
+</script>
+
+<style lang="css">
+</style>
