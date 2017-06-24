4be194a58c3cdf511e7832f2146bc30ec6907576
Palakorn <palakorn.plo@ascendcorp.com>
2017-06-24 16:34:03 +0700

    update Trip view

--- src/views/MyTrips.vue
+++ src/views/MyTrips.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>My Trips page</div>
+  <div>My Trips page
+    <TripList :trips="tripsVar"></TripList>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'myTrips'
-}
+  import TripList from '@/components/TripList'
+  export default {
+    name: 'myTrips',
+    components: {
+      TripList
+    },
+    methods: {
+      fetchTrips: function () {
+        console.log('fetchTrips')
+      }
+    },
+    data () {
+      return {
+        tripsVar: [{
+          id: '1',
+          img: 'http://cdn.www.ministry-to-children.com/wp-content/uploads/2015/07/family-road-trip.jpg',
+          name: 'trip1',
+          date: '02 aug 2017'
+        },
+        {
+          id: '2',
+          img: 'http://cdn.www.ministry-to-children.com/wp-content/uploads/2015/07/family-road-trip.jpg',
+          name: 'trip2',
+          date: '03 aug 2017'
+        }
+        ]
+      }
+    }
+  }
 </script>
 
 <style>
