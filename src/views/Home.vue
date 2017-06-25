0772398f71de0166e684da5bfabf230f91b80343
Teerasak Vichadee <teerasak.vic@ascendcorp.com>
2017-06-25 09:20:46 +0700

    REMOVE unused code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,15 @@
   <div class="main-layout">
     <div>
       <form>
-        <AutoComplete @value="getStartAt"></AutoComplete>
-        <br>
-        <AutoComplete @value="getDestination"></AutoComplete>
-        <br>
-        <b-button class="my-button" @click.prevent="getFormValues()">Submit</b-button>
-        <hr>
         <LocationSearch
           placeholder="Enter starting point"
           @update:location="location => startAt = location">
         </LocationSearch>
-        start at = {{startAt}}
+        <LocationSearch
+          placeholder="Enter destination point"
+          @update:location="location => destination = location">
+        </LocationSearch>
+        <b-button variant="primary" @click.prevent="createTrip()">Create Trip</b-button>
       </form>
     </div>
     <hr>
@@ -44,65 +42,21 @@ export default {
     }
   },
   methods: {
-    prepareLocation: function (locations, loc, order) {
-      locations[loc.place_id] = {
-        name: loc.name,
-        order: order,
-        rating: (loc.rating) ? loc.rating : 0,
-        thumbnail: (loc.photos) ? loc.photos[0].photo_reference : '',
-        position: {
-          lat: loc.geometry.location.lat,
-          lng: loc.geometry.location.lng
-        }
-      }
-      return locations
-    },
-    addTrip: function (locations) {
-      var vm = this
-      var userData = getUser()
-      var data = {
-        name: vm.startAt + ' - ' + vm.destination,
-        date: '2017-06-24 11:00:00',
-        locations: locations
-      }
-      return db.ref('/trips/' + userData.uid).push(data).key
-    },
-    getFormValues: function () {
-      var vm = this
-      if (typeof vm.startAt === 'undefined' || typeof vm.destination === 'undefined' || vm.startAt.length === 0 || vm.destination.length === 0) {
-        alert('Please input origin and destination location')
-        return
-      }
-      var locations = {}
-      searchPlace(vm.startAt).then(function (response) {
-        if (response.data.results.length > 0) {
-          locations = vm.prepareLocation(locations, response.data.results[0], 0)
-          searchPlace(vm.destination).then(function (response) {
-            if (response.data.results.length > 0) {
-              locations = vm.prepareLocation(locations, response.data.results[0], 9999)
-              let tripId = vm.addTrip(locations)
-              vm.$router.push('/trips/' + tripId)
-            } else {
-              alert('Sorry, we cannot find your destination location.')
-              return
-            }
-          })
-        } else {
-          alert('Sorry, we cannot find your origin location.')
-          return
-        }
-      })
-    },
-    getStartAt: function (val) {
-      this.startAt = val
-    },
-    getDestination: function (val) {
-      this.destination = val
-    },
-    firebase: function () {
-      return {
-        trips: db.ref('trips')
-      }
+    createTrip () {
+      this.startAt.order = 0
+      this.destination.order = 1
+
+      let locations = {}
+      locations[this.startAt.id] = this.startAt
+      locations[this.destination.id] = this.destination
+
+      const trip_id = db.ref('/trips/' + getUser().uid).push({
+        name: this.startAt.name + ' - ' + this.destination.name,
+        date: new Date().toLocaleString(),
+        locations
+      }).key
+
+      this.$router.push('/trips/' + trip_id)
     }
   },
 
@@ -119,12 +73,6 @@ export default {
   margin: auto;
   text-align: center;
 }
-/*.v-autocomplete {
-  float: none !important;
-}
-.v-autocomplete-input {
-  width: 60% !important;
-}*/
 .my-button {
   width: 100px;
   background-color: #138d75;
